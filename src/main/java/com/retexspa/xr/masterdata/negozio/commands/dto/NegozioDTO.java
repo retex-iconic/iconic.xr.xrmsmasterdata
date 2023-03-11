48fe47925ae23b907c71abe6d95c4cf78266a131
Daniele Favara <daniele.favara@retexspa.com>
2023-03-11 09:49:42 +0100

    feat: :construction_worker: enable missing fields based on mapper

--- src/main/java/com/retexspa/xr/masterdata/negozio/commands/dto/NegozioDTO.java
+++ src/main/java/com/retexspa/xr/masterdata/negozio/commands/dto/NegozioDTO.java
@@ -10,6 +10,80 @@ public class NegozioDTO {
 
   private String master;
 
+  private String magazzino;
+
+  private String differenzialePartitaIVA;
+
+  private String nazionePartitaIVA;
+
+  private String partitaIVA;
+
+  private String reaUfficio;
+
+  private String reaNumero;
+
+  private String reaCapitaleSociale;
+
+  private String reaSocioUnico;
+
+  private String reaLiquidazione;
+
+  private String codiceDestinatarioSDI;
+
+  private String sedeLegaleIndirizzo;  
+
+  private String sedeLegaleNumeroCivico;
+
+  private String sedeLegaleCAP;
+
+  private String sedeLegaleComune;
+
+  private String sedeLegaleProvincia;
+
+  private String sedeLegaleNazione;
+
+  private String sedeOperativaNome;
+
+  private String sedeOperativaIndirizzo;
+
+  private String sedeOperativaNumeroCivico;
+
+  private String sedeOperativaCAP;
+
+  private String sedeOperativaComune;
+
+  private String sedeOperativaProvincia;
+
+  private String sedeOperativaNazione;
+
+  private String telefono;
+
+  private String cellulare;
+
+  private String fax;
+
+  private String email;
+
+  private String pec;
+
+  private String web;
+
+  private String iban;
+
+  private String nazioneTrasmittente;
+
+  private String partitaIvaTrasmittente;
+
+  private String codiceAteco;
+
+  private String canale;
+
+  private String insegna;
+
+  private String ivaVentilata;
+
+
+
   public String getRagioneSociale() {
     return this.ragioneSociale;
   }
@@ -41,4 +115,292 @@ public class NegozioDTO {
   public void setMaster(String master) {
     this.master = master;
   }
+
+  public String getMagazzino() {
+    return this.magazzino;
+  }
+
+  public void setMagazzino(String magazzino) {
+    this.magazzino = magazzino;
+  }
+
+  public String getDifferenzialePartitaIVA() {
+    return this.differenzialePartitaIVA;
+  }
+
+  public void setDifferenzialePartitaIVA(String differenzialePartitaIVA) {
+    this.differenzialePartitaIVA = differenzialePartitaIVA;
+  }
+
+  public String getNazionePartitaIVA() {
+    return this.nazionePartitaIVA;
+  }
+
+  public void setNazionePartitaIVA(String nazionePartitaIVA) {
+    this.nazionePartitaIVA = nazionePartitaIVA;
+  }
+
+  public String getPartitaIVA() {
+    return this.partitaIVA;
+  }
+
+  public void setPartitaIVA(String partitaIVA) {
+    this.partitaIVA = partitaIVA;
+  }
+
+  public String getReaUfficio() {
+    return this.reaUfficio;
+  }
+
+  public void setReaUfficio(String reaUfficio) {
+    this.reaUfficio = reaUfficio;
+  }
+
+  public String getReaNumero() {
+    return this.reaNumero;
+  }
+
+  public void setReaNumero(String reaNumero) {
+    this.reaNumero = reaNumero;
+  }
+
+  public String getReaCapitaleSociale() {
+    return this.reaCapitaleSociale;
+  }
+
+  public void setReaCapitaleSociale(String reaCapitaleSociale) {
+    this.reaCapitaleSociale = reaCapitaleSociale;
+  }
+
+  public String getReaSocioUnico() {
+    return this.reaSocioUnico;
+  }
+
+  public void setReaSocioUnico(String reaSocioUnico) {
+    this.reaSocioUnico = reaSocioUnico;
+  }
+
+  public String getReaLiquidazione() {
+    return this.reaLiquidazione;
+  }
+
+  public void setReaLiquidazione(String reaLiquidazione) {
+    this.reaLiquidazione = reaLiquidazione;
+  }
+
+  public String getCodiceDestinatarioSDI() {
+    return this.codiceDestinatarioSDI;
+  }
+
+  public void setCodiceDestinatarioSDI(String codiceDestinatarioSDI) {
+    this.codiceDestinatarioSDI = codiceDestinatarioSDI;
+  }
+
+  public String getSedeLegaleIndirizzo() {
+    return this.sedeLegaleIndirizzo;
+  }
+
+  public void setSedeLegaleIndirizzo(String sedeLegaleIndirizzo) {
+    this.sedeLegaleIndirizzo = sedeLegaleIndirizzo;
+  }
+
+  public String getSedeLegaleNumeroCivico() {
+    return this.sedeLegaleNumeroCivico;
+  }
+
+  public void setSedeLegaleNumeroCivico(String sedeLegaleNumeroCivico) {
+    this.sedeLegaleNumeroCivico = sedeLegaleNumeroCivico;
+  }
+
+  public String getSedeLegaleCAP() {
+    return this.sedeLegaleCAP;
+  }
+
+  public void setSedeLegaleCAP(String sedeLegaleCAP) {
+    this.sedeLegaleCAP = sedeLegaleCAP;
+  }
+
+  public String getSedeLegaleComune() {
+    return this.sedeLegaleComune;
+  }
+
+  public void setSedeLegaleComune(String sedeLegaleComune) {
+    this.sedeLegaleComune = sedeLegaleComune;
+  }
+
+  public String getSedeLegaleProvincia() {
+    return this.sedeLegaleProvincia;
+  }
+
+  public void setSedeLegaleProvincia(String sedeLegaleProvincia) {
+    this.sedeLegaleProvincia = sedeLegaleProvincia;
+  }
+
+  public String getSedeLegaleNazione() {
+    return this.sedeLegaleNazione;
+  }
+
+  public void setSedeLegaleNazione(String sedeLegaleNazione) {
+    this.sedeLegaleNazione = sedeLegaleNazione;
+  }
+
+  public String getSedeOperativaNome() {
+    return this.sedeOperativaNome;
+  }
+
+  public void setSedeOperativaNome(String sedeOperativaNome) {
+    this.sedeOperativaNome = sedeOperativaNome;
+  }
+
+  public String getSedeOperativaIndirizzo() {
+    return this.sedeOperativaIndirizzo;
+  }
+
+  public void setSedeOperativaIndirizzo(String sedeOperativaIndirizzo) {
+    this.sedeOperativaIndirizzo = sedeOperativaIndirizzo;
+  }
+
+  public String getSedeOperativaNumeroCivico() {
+    return this.sedeOperativaNumeroCivico;
+  }
+
+  public void setSedeOperativaNumeroCivico(String sedeOperativaNumeroCivico) {
+    this.sedeOperativaNumeroCivico = sedeOperativaNumeroCivico;
+  }
+
+  public String getSedeOperativaCAP() {
+    return this.sedeOperativaCAP;
+  }
+
+  public void setSedeOperativaCAP(String sedeOperativaCAP) {
+    this.sedeOperativaCAP = sedeOperativaCAP;
+  }
+
+  public String getSedeOperativaComune() {
+    return this.sedeOperativaComune;
+  }
+
+  public void setSedeOperativaComune(String sedeOperativaComune) {
+    this.sedeOperativaComune = sedeOperativaComune;
+  }
+
+  public String getSedeOperativaProvincia() {
+    return this.sedeOperativaProvincia;
+  }
+
+  public void setSedeOperativaProvincia(String sedeOperativaProvincia) {
+    this.sedeOperativaProvincia = sedeOperativaProvincia;
+  }
+
+  public String getSedeOperativaNazione() {
+    return this.sedeOperativaNazione;
+  }
+
+  public void setSedeOperativaNazione(String sedeOperativaNazione) {
+    this.sedeOperativaNazione = sedeOperativaNazione;
+  }
+
+  public String getTelefono() {
+    return this.telefono;
+  }
+
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  public String getCellulare() {
+    return this.cellulare;
+  }
+
+  public void setCellulare(String cellulare) {
+    this.cellulare = cellulare;
+  }
+
+  public String getFax() {
+    return this.fax;
+  }
+
+  public void setFax(String fax) {
+    this.fax = fax;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPec() {
+    return this.pec;
+  }
+
+  public void setPec(String pec) {
+    this.pec = pec;
+  }
+
+  public String getWeb() {
+    return this.web;
+  }
+
+  public void setWeb(String web) {
+    this.web = web;
+  }
+
+  public String getIban() {
+    return this.iban;
+  }
+
+  public void setIban(String iban) {
+    this.iban = iban;
+  }
+
+  public String getNazioneTrasmittente() {
+    return this.nazioneTrasmittente;
+  }
+
+  public void setNazioneTrasmittente(String nazioneTrasmittente) {
+    this.nazioneTrasmittente = nazioneTrasmittente;
+  }
+
+  public String getPartitaIvaTrasmittente() {
+    return this.partitaIvaTrasmittente;
+  }
+
+  public void setPartitaIvaTrasmittente(String partitaIvaTrasmittente) {
+    this.partitaIvaTrasmittente = partitaIvaTrasmittente;
+  }
+
+  public String getCodiceAteco() {
+    return this.codiceAteco;
+  }
+
+  public void setCodiceAteco(String codiceAteco) {
+    this.codiceAteco = codiceAteco;
+  }
+
+  public String getCanale() {
+    return this.canale;
+  }
+
+  public void setCanale(String canale) {
+    this.canale = canale;
+  }
+
+  public String getInsegna() {
+    return this.insegna;
+  }
+
+  public void setInsegna(String insegna) {
+    this.insegna = insegna;
+  }
+
+  public String getIvaVentilata() {
+    return this.ivaVentilata;
+  }
+
+  public void setIvaVentilata(String ivaVentilata) {
+    this.ivaVentilata = ivaVentilata;
+  }  
 }
