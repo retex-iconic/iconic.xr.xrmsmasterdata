0e007cf4d6ca207c4ec3bb0ba0120174311e3af7
Daniele Favara <daniele.favara@retexspa.com>
2023-03-11 09:27:08 +0100

    feat: :construction_worker: comment out unmapped keys and, to enable required ones add them to ArticoloDTO and ArticoloQueryEntity

--- src/main/java/com/retexspa/xr/masterdata/articolo/entities/ArticoloMapper.java
+++ src/main/java/com/retexspa/xr/masterdata/articolo/entities/ArticoloMapper.java
@@ -5,30 +5,45 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import com.retexspa.xr.masterdata.articolo.commands.dto.ArticoloDTO;
 
+/* 
+
+Create all the target properties in ArticoloDTO and ArticoloQueryEntity
+
+ only fields with block comment as prefix  are required 
+
+ */
+
 @Mapper
 public interface ArticoloMapper {
 
    ArticoloMapper MAPPER = Mappers.getMapper( ArticoloMapper.class );
 
-   @Mapping(target = "articolo", source = "articolo")
+   @Mapping(target = "codice2", source = "articolo")
+
 // @Mapping(source = "codicepri", ignore = true")
+
    @Mapping(target = "reparto", source = "reparto")
    @Mapping(target = "gruppo", source = "gruppo")
+
 // @Mapping(source = "sospeso", ignore = true")
 // @Mapping(source = "datins", ignore = true")
 // @Mapping(source = "iva", ignore = true")
-   @Mapping(target = "descrizione", source = "descriz")
-   @Mapping(target = "descrizioneBreve", source = "desbrev")
-   @Mapping(target = "unitaDiMisura", source = "misura")
-   @Mapping(target = "quantita", source = "quantita")
-   @Mapping(target = "scortaMinima", source = "scorta")
+
+//* */ @Mapping(target = "descrizione", source = "descriz")
+//* */ @Mapping(target = "descrizioneBreve", source = "desbrev")
+//* */ @Mapping(target = "unitaDiMisura", source = "misura")
+//* */ @Mapping(target = "quantita", source = "quantita")
+//* */ @Mapping(target = "scortaMinima", source = "scorta")
+
 // @Mapping(source = "giaciniz", ignore = true")
 // @Mapping(source = "valiniz", ignore = true")
 // @Mapping(source = "ricarico", ignore = true")
 // @Mapping(source = "ricaric2", ignore = true")
-   @Mapping(target = "deposito", source = "deposito")
-   @Mapping(target = "scaffale", source = "scaffale")
-   @Mapping(target = "superficieEspositiva", source = "supespo")
+
+//* */ @Mapping(target = "deposito", source = "deposito")
+//* */ @Mapping(target = "scaffale", source = "scaffale")
+//* */ @Mapping(target = "superficieEspositiva", source = "supespo")
+
 // @Mapping(source = "offspec", ignore = true")
 // @Mapping(source = "datinof", ignore = true")
 // @Mapping(source = "datfiof", ignore = true")
@@ -53,7 +68,9 @@ public interface ArticoloMapper {
 // @Mapping(source = "gioroff", ignore = true")
 // @Mapping(source = "quascart", ignore = true")
 // @Mapping(source = "valscart", ignore = true")
-   @Mapping(target = "quantitaImpegnata", source = "quaimpeg")
+
+//* */ @Mapping(target = "quantitaImpegnata", source = "quaimpeg")
+
 // @Mapping(source = "giacmed", ignore = true")
 // @Mapping(source = "varprv", ignore = true")
 // @Mapping(source = "quaulve", ignore = true")
@@ -63,12 +80,16 @@ public interface ArticoloMapper {
 // @Mapping(source = "datinv", ignore = true")
 // @Mapping(source = "datvar", ignore = true")
 // @Mapping(source = "datcons", ignore = true")
-   @Mapping(target = "scortaMassima", source = "scortamax")
-   @Mapping(target = "layoutEtichetta", source = "etilay")
+
+//* */ @Mapping(target = "scortaMassima", source = "scortamax")
+//* */ @Mapping(target = "layoutEtichetta", source = "etilay")
+
 // @Mapping(source = "dateti", ignore = true")
 // @Mapping(source = "prcons", ignore = true")
 // @Mapping(source = "diffinv", ignore = true")
+
    @Mapping(target = "produttore", source = "produttore")
+
 // @Mapping(source = "ultprli", ignore = true")
 // @Mapping(source = "ultprsc", ignore = true")
 // @Mapping(source = "ultprsi", ignore = true")
@@ -78,11 +99,17 @@ public interface ArticoloMapper {
 // @Mapping(source = "prpers", ignore = true")
 // @Mapping(source = "prdel", ignore = true")
 // @Mapping(source = "descedi", ignore = true")
-   @Mapping(target = "tipoProdotto", source = "tipoart")
+
+//* */ @Mapping(target = "tipoProdotto", source = "tipoart")
+
 // @Mapping(source = "fidpromo", ignore = true")
-   @Mapping(target = "numeroEtichette", source = "numeti")
+
+//* */ @Mapping(target = "numeroEtichette", source = "numeti")
+
 // @Mapping(source = "apeso", ignore = true")
-   @Mapping(target = "tipoScaricoVenduto", source = "tiposcar")
+
+//* */ @Mapping(target = "tipoScaricoVenduto", source = "tiposcar")
+
 // @Mapping(source = "preuro", ignore = true")
 // @Mapping(source = "datmod", ignore = true")
 // @Mapping(source = "datcas", ignore = true")
@@ -98,26 +125,36 @@ public interface ArticoloMapper {
 // @Mapping(source = "qtaord", ignore = true")
 // @Mapping(source = "prord", ignore = true")
 // @Mapping(source = "medvendgg", ignore = true")
-   @Mapping(target = "fornitorePrimario", source = "fornpri")
-   @Mapping(target = "fornitoreEtichetta", source = "forneti")
+
+//* */ @Mapping(target = "fornitorePrimario", source = "fornpri")
+//* */ @Mapping(target = "fornitoreEtichetta", source = "forneti")
+
 // @Mapping(source = "flgecom", ignore = true")
 // @Mapping(source = "giacwarn", ignore = true")
 // @Mapping(source = "giacelim", ignore = true")
-   @Mapping(target = "posizionamento", source = "codposiz")
-   @Mapping(target = "stagionalita", source = "codstag")
-   @Mapping(target = "tipoProdotto2", source = "codtipo2")
+
+//* */ @Mapping(target = "posizionamento", source = "codposiz")
+//* */ @Mapping(target = "stagionalita", source = "codstag")
+//* */ @Mapping(target = "tipoProdotto2", source = "codtipo2")
+
 // @Mapping(source = "subtotsc", ignore = true")
 // @Mapping(source = "subtotpt", ignore = true")
-   @Mapping(target = "codiceTLCentrale", source = "artcedi")
+
+//* */ @Mapping(target = "codice", source = "artcedi")
+
 // @Mapping(source = "artbo", ignore = true")
 // @Mapping(source = "datws", ignore = true")
 // @Mapping(source = "przblk", ignore = true")
 // @Mapping(source = "przimp", ignore = true")
-   @Mapping(target = "svalorizzazione", source = "codsva")
-   @Mapping(target = "cluster", source = "cluster")
-   @Mapping(target = "assortimentoPuntoVendita", source = "asspv")
+
+//* */ @Mapping(target = "svalorizzazione", source = "codsva")
+//* */ @Mapping(target = "cluster", source = "cluster")
+//* */ @Mapping(target = "assortimentoPuntoVendita", source = "asspv")
+
 // @Mapping(source = "idsendecr", ignore = true")
-   @Mapping(target = "layoutESL", source = "esllay")
+
+//* */ @Mapping(target = "layoutESL", source = "esllay")
+
 // @Mapping(source = "esllpro", ignore = true")
 // @Mapping(source = "dataoraupd", ignore = true")
 // @Mapping(source = "datim", ignore = true")
